0ed178bec28010fd992467a8103c8e1c0f96966d
ameedfaridi <faridiameed5@gmail.com>
2020-09-03 17:56:48 +0530

    Updates

---
+++ precache-manifest.8ba6699c57a0219ba64ebd5db84bbb6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870401cd22eb83bbc446f42e724b223f",
+    "url": "/Vyorius/index.html"
+  },
+  {
+    "revision": "a40ceda283a356c664a7",
+    "url": "/Vyorius/static/css/main.5f0cc39a.chunk.css"
+  },
+  {
+    "revision": "589fcebf271f93ac3ac0",
+    "url": "/Vyorius/static/js/2.518a614f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Vyorius/static/js/2.518a614f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a40ceda283a356c664a7",
+    "url": "/Vyorius/static/js/main.922602c0.chunk.js"
+  },
+  {
+    "revision": "a1cdefe1fa723104b300",
+    "url": "/Vyorius/static/js/runtime-main.e094e9d2.js"
+  },
+  {
+    "revision": "6975ce3ce2b16e0cea547bcee888cee8",
+    "url": "/Vyorius/static/media/img.6975ce3c.png"
+  }
+]);
